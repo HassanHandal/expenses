cf02cdaa5f8a128ee605cd0b88a89bc15f67a861
HassanHandal <hassanhamdyfarid@gmail.com>
2023-12-24 22:36:24 +0300

    final

--- expenses.py
+++ expenses.py
@@ -1,3 +1,4 @@
+import requests
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry
@@ -6,6 +7,9 @@ currency_list = []
 category_list = []
 payment_list = []
 date_list = []
+def show_error_message(message):
+    error_label.config(text=message)
+
 def is_integer(value):
     try:
         int(value)
@@ -21,16 +25,46 @@ def add_expense():
     t3 = so2.get()
     t4 = so3.get()
     t5 = dt.get()
-    if is_integer(t1) or t1 <= 0 or not_select(t2) or not_select(t3) or not_select(t4):
+    if is_integer(t1) and int(t1) > 0  and t1!="" and not_select(t2) and not_select(t3) and not_select(t4):
+        if error_label.cget("text") != "":
+            error_label.config(text="")
+        if amount_list:
+            amount_list.pop()
+        if currency_list:
+            currency_list.pop()
         amount_list.append(t1)
         currency_list.append(t2)
         category_list.append(t3)
         payment_list.append(t4)
         date_list.append(t5)
+        total_amount = 0
+        for b in range(len(amount_list)):
+            from_curr = currency_list[b]
+            to_curr = "USD"
+            amount = amount_list[b]
+            url = f"https://api.apilayer.com/fixer/convert?to={to_curr}&from={from_curr}&amount={amount}"
+
+            headers = {
+                "apikey": "DMunyZazMOy2HKxKCkh7EfRSWdOtyoX3"
+            }
+
+            response = requests.get(url, headers=headers)
+
+            status_code = response.status_code
+            result = response.json()
+            total_amount += round(float(result["result"]),2)
+        amount_list.append(round(float(total_amount),2))
+        currency_list.append("USD")
         for item in tree.get_children():
             tree.delete(item)
-        for i in range(len(amount_list)):
+        for i in range(len(payment_list)):
             tree.insert("", "end", values=(amount_list[i], currency_list[i], category_list[i], payment_list[i], date_list[i]))
+        tag_name = f"tag{len(amount_list)}"
+        tree.insert("", "end", values=(amount_list[len(amount_list)-1], currency_list[len(amount_list)-1]),tags=tag_name)
+        tree.tag_configure(tag_name, background="yellow",font=('TkDefaultFont', 9, 'bold'))
+    else:
+        show_error_message("Invalid input. Please check your entries.")
+
     
   
 
@@ -105,14 +139,15 @@ dt.grid(column = 1,row = 4,padx=1,pady=10)
 button = tk.Button(window,text="Add Expense",command=add_expense)
 button.grid(column=0,row=1,sticky="ew")
 tree = ttk.Treeview(window,columns=("Amount","Currency","Category","Payment Method","Date"),show="headings")
-tree.heading("Amount",text="Amount")
-tree.heading("Currency",text="Currency")
-tree.heading("Category",text="Category")
-tree.heading("Payment Method",text="Payment Method")
-tree.heading("Date",text="Date")
+for col in ("Amount", "Currency", "Category", "Payment Method", "Date"):
+    tree.heading(col, text=col, anchor="center")
+    tree.column(col, anchor="center")
 tree.grid(column=0,row=2,sticky="nsew")
 scrollbar = ttk.Scrollbar(window, orient='vertical', command=tree.yview)
 scrollbar.grid(row=2, column=1, sticky='ns')
+error_label = tk.Label(window, text="", foreground="red")
+error_label.grid(column=0, row=3, sticky="ew")
+
 window.columnconfigure(0, weight=1)
 window.rowconfigure(2, weight=2)
 lframe.columnconfigure(0,weight=1)
